a2ddb63f2a400435e5ebca7804f4ade3e63187d3
Oktut <ya.oktut@yandex.ru>
2023-08-01 07:28:44 +0300

    7

--- server.py
+++ server.py
@@ -1,35 +1,47 @@
 import socket
-import queue
 import heapq
 
+
 def perform_operation(operator, num1, num2):
-    # Реализация perform_operation остается без изменений
-    # ...
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            raise ValueError("Division by zero is not allowed.")
+    else:
+        raise ValueError("Unsupported operator.")
+
 
 def main():
     host = "127.0.0.1"
     port = 5000
 
-    # Create a UDP socket
+    # Создаем UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
         server_socket.bind((host, port))
         print(f"Server started and listening on {host}:{port}")
 
-        operation_queue = []  # Очередь с приоритетами на базе кучи (heap)
+        operation_queue = []  # Очередь с приоритетами (heap)
 
         while True:
             try:
-                # Receive data from the client
+                # Получение данных от клиента
                 data, client_address = server_socket.recvfrom(1024)
                 data = data.decode("utf-8")
 
-                # Parse the data received from the client
+                # Разбираем полученные данные от клиента
                 operator, num1, num2 = data.split()
                 num1 = float(num1)
                 num2 = float(num2)
 
                 try:
-                    # Perform the operation
+                    # Выполняем операцию
                     result = perform_operation(operator, num1, num2)
                     response = str(result).encode("utf-8")
                 except ValueError as ve:
@@ -48,6 +60,7 @@ def main():
             except KeyboardInterrupt:
                 print("\nServer stopped.")
                 break
+
 
 if __name__ == "__main__":
     main()
